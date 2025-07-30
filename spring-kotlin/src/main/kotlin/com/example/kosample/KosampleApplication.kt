f2c719519e9f7804a634f6bd2cbab78f9030ac60
min-gui <msj349911@gmail.com>
2025-07-30 19:45:17 +0900

    feat : kotlin project first commit

---
+++ spring-kotlin/src/main/kotlin/com/example/kosample/KosampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kosample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KosampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<KosampleApplication>(*args)
+}
---
+++ spring-kotlin/src/test/kotlin/com/example/kosample/KosampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kosample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KosampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
